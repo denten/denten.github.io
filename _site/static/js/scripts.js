1361a4171e08e694a25a6a83f4c64401ae298f50
denten <denten@gmail.com>
2014-02-02 00:14:42 -0500

    adding site

---
+++ _site/static/js/scripts.js
@@ -0,0 +1,10 @@
+/*
+    Diverse scripts
+*/
+
+// track disqus comments via google analytics
+function disqus_config() {
+    this.callbacks.onNewComment = [function() {
+        _gaq.push(['_trackEvent', 'Comment']);
+    }];
+}
